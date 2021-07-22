c2e9cd29052a4c22b6f6e5df78f63bd8bb1deac5
Aluno <Aluno@LAB2-04>
2021-07-22 14:06:54 -0300

    Adicionar arquivos de projeto.

---
+++ algoritmo/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace algoritmo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Valores valor = new Valores();
+            Console.WriteLine("Qual seu nome?");
+            valor.Nome = Console.ReadLine();
+            Console.WriteLine("Qual sua idade?");
+            valor.Idade = int.Parse(Console.ReadLine());
+            if (valor.Idade < 18)
+            {
+                Console.WriteLine("Qual o nome da sua mãe?");
+                valor.Mae = Console.ReadLine();
+                Console.WriteLine("nome: {0}, idade: {1}, nome da mae: {2}", valor.Nome, valor.Idade, valor.Mae);
+            }
+            else
+            {
+                Console.WriteLine("nome: {0}, idade: {1}", valor.Nome, valor.Idade);
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ algoritmo/Valores.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace algoritmo
+{
+    class Valores
+    {
+        public string Nome { get; set; }
+        public int Idade { get; set; }
+        public string Mae { get; set; }
+    }
+}
